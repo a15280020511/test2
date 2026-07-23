44554dfd6c8cac8591322185d8f41e76e7ba9ffd
a15280020511 <a15280020511@gmail.com>
2026-07-24 07:38:11 +0800

    Expose dynamic expert team API

---
+++ expert_team/__init__.py
@@ -0,0 +1,5 @@
+"""Dynamic expert-team package."""
+
+from .dynamic_team import ExpertSpec, TeamPlan, plan_team, run_dynamic_team
+
+__all__ = ["ExpertSpec", "TeamPlan", "plan_team", "run_dynamic_team"]
